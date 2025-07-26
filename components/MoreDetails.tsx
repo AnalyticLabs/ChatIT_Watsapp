a406acbaf8a6b251476394c5dfb4e26fe009930a
Dibyajyoti Sahoo <intern3.analyticlabs@gmail.com>
2025-07-26 22:14:26 +0530

    add logout functionality to MoreDetails component

--- components/MoreDetails.tsx
+++ components/MoreDetails.tsx
@@ -3,20 +3,38 @@ import { View, Text, TouchableOpacity, Pressable } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { useTheme } from "@react-navigation/native";
 import { useColorScheme } from "~/lib/useColorScheme";
+import { useRouter } from "expo-router";
+import { logout } from "~/features/auth/authAction";
+import { useAppDispatch } from "~/store";
 
 export function MoreDetails() {
   const { colors } = useTheme();
   const { isDarkColorScheme } = useColorScheme();
   const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+  const dispatch = useAppDispatch();
 
   const menuItems: {
     label: string;
-    icon: "users" | "volume-2" | "star" | "settings";
+    icon: "users" | "volume-2" | "star" | "settings" | "log-out";
+    onPress?: () => void | Promise<void>;
   }[] = [
     { label: "New group", icon: "users" },
     { label: "New broadcast", icon: "volume-2" },
     { label: "Starred messages", icon: "star" },
     { label: "Settings", icon: "settings" },
+    {
+      label: "Logout",
+      icon: "log-out",
+      onPress: async () => {
+        await dispatch(logout());
+        console.log("Auth cleared. Navigating to /");
+
+        setTimeout(() => {
+          router.replace("/");
+        }, 100);
+      },
+    },
   ];
 
   return (
@@ -30,12 +48,12 @@ export function MoreDetails() {
           onPressOut={() => setIsOpen(false)}
           className="absolute right-0 top-10 w-52 bg-[#fcfcfc] dark:bg-gray-800 p-2 rounded-xl shadow-lg shadow-gray-700 z-50"
         >
-          {menuItems.map(({ label, icon }, index) => (
+          {menuItems.map(({ label, icon, onPress }, index) => (
             <TouchableOpacity
               key={index}
-              onPress={() => {
+              onPress={async () => {
                 setIsOpen(false);
-                // Handle your item action here
+                if (onPress) await onPress();
               }}
               className="flex-row items-center gap-2 p-2 rounded-md active:bg-neutral-200 dark:active:bg-neutral-700"
             >
